986b3f62bfb490850d31307bef7f0ae7c14d6b80
GordonYuanyc <gordonyuanyc@gmail.com>
2018-06-20 11:00:28 -0500

    try tail_recursion next time

--- factorial_recursion.c
+++ factorial_recursion.c
@@ -13,12 +13,17 @@ int main(){
 }
 
 int calculate_fact(int num,int result){
+  // printf("num is: %d",num);
+  int k = num;
   int re = result;
-  if (num = 1){
-    return num*re;
+  if (k == 0){
+    return result;
   }
   else{
-    re = calculate_fact(num--,result);
-    return num*re;
+    //re = calculate_fact(num--,result);
+    //printf("re is : %d",re);
+    //printf("num-- is: %d",num--);
+    int id = k-1;
+    return k*calculate_fact(id,result);
   }
 }
